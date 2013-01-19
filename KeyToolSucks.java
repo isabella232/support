ad33f955e81d36fb408d7690642b570ee6668d18
Kurt Rose <kurt@kurtrose.com>
2013-01-18 19:22:13 -0800

    updating KeyToolSucks to (hopefully) replace openssl and keytool dependencies completely

--- KeyToolSucks.java
+++ KeyToolSucks.java
@@ -6,6 +6,7 @@ That's right, I said it.  KeyTool sucks.
 import java.security.KeyStore;
 import java.io.FileInputStream;
 import java.util.Enumeration;
+import java.util.ArrayList;
 import java.security.UnrecoverableKeyException;
 
 public class KeyToolSucks {
@@ -13,24 +14,45 @@ public class KeyToolSucks {
         KeyStore keystore = KeyStore.getInstance("jks");
         char[] password = args[1].toCharArray();
         keystore.load(new FileInputStream(args[0]), password);
-        byte[] cert_bytes = null;
+        ArrayList<byte[]> trusted_cert_bytes = new ArrayList<byte[]>();
+        byte[] pkey_bytes = null;
+        byte[] pub_cert_bytes = null;
         for(Enumeration<String> aliases = keystore.aliases(); aliases.hasMoreElements();) {
             String nxt = aliases.nextElement();
             try {
-                cert_bytes = ((KeyStore.TrustedCertificateEntry)keystore.getEntry(
-                    nxt, null)).getTrustedCertificate().getEncoded();  //lol, Java
+                trusted_cert_bytes.add(((KeyStore.TrustedCertificateEntry)keystore.getEntry(
+                    nxt, null)).getTrustedCertificate().getEncoded());  //lol, Java
             } catch (Exception e) { //exception that gets raised varies between environments
                 cert_bytes = ((KeyStore.PrivateKeyEntry)keystore.getEntry(
                     nxt, new KeyStore.PasswordProtection(password))
                     ).getCertificate().getEncoded();
             }
-
-            for(int j=0; j<cert_bytes.length; j++) {
-                System.out.print(String.format("%02X", cert_bytes[j]));
-            }
-            System.out.print("\n");
         }
+        //print out JSON formatted data
+        System.out.println("{");
+        System.out.print("\"pub_cert_bytes\":");
+        print_bytes(pub_cert_bytes);
+        System.out.println(",");
+        System.out.print("\"pkey_bytes\":");
+        print_bytes(pkey_bytes);
+        System.out.println(",");
+        System.out.print("\"trusted_cert_bytes\":[");
+        for(int i=0; i<trusted_cert_bytes.size(); i++) {
+            print_bytes(trusted_cert_bytes.get(i));
+            if(i !=  trusted_cert_bytes.size() -1) {
+                System.out.println(",");
+            }
+        }
+        System.out.print("]\n}\n");
         System.out.flush();
+    }
+
+    private static void print_bytes(byte[] b) {
+        System.out.print("\"");
+        for(int j=0; j<b.length; j++) {
+            System.out.print(String.format("%02X", b[j]));
+        }
+        System.out.print("\"");
     }
 
 }
