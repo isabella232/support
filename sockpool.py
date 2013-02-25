62da0a5c265d656b840d69bbd9a69c8fab62e11f
Kurt Rose <kurt@kurtrose.com>
2013-02-25 15:39:18 -0800

    setting up clients to use sock pools (fingers crossed)

--- sockpool.py
+++ sockpool.py
@@ -1,6 +1,6 @@
 import socket
 import time
-import gevent.select
+import select
 
 class SockPool(object):
     def __init__(self, timeout=0.25):
@@ -10,6 +10,7 @@ class SockPool(object):
 
     def acquire(self, addr):
         #return a free socket, if one is availble; else None
+        self.cull()
         socks = self.free_socks_by_addr.get(addr)
         if socks:
             sock = socks.pop()
@@ -20,6 +21,12 @@ class SockPool(object):
     def release(self, sock):
         #this is also a way of "registering" a socket with the pool
         #basically, this says "I'm done with this socket, make it available for anyone else"
+        if select.select([self.sock], [], [], 0)[0]:
+            killsock(self.sock)
+            return #TODO: raise exception when handed messed up socket?
+            #socket is readable means one of two things:
+            #1- left in a bad state (e.g. more data waiting -- protocol state is messed up)
+            #2- socket closed by remote (in which case read will return empty string)
         addr = sock.getpeername()
         self.free_socks_by_addr.setdefault(addr, []).append(sock)
         self.sock_idle_times[sock] = time.time()
@@ -41,9 +48,6 @@ class SockPool(object):
         for sock in culled:
             killsock(sock)
 
-    def ctx_sock(self, addr, create_sock):
-        return CtxSock(self, addr, create_sock)
-
 def killsock(sock):
     try:
         sock.shutdown(socket.SHUT_RDWR)
@@ -53,31 +57,4 @@ def killsock(sock):
         sock.close()
     except socket.error:
         pass #just being nice to the server, don't care if it fails
-
-class CtxSock(object):
-    def __init__(self, pool, addr, create_sock):
-        self.pool = pool
-        self.addr = addr
-        self.create_sock = create_sock
-
-    def __enter__(self):
-        self.sock = self.pool.acquire(self.addr) or self.create_sock(self.addr)
-
-    def __exit__(self, exc_type, exc_value, traceback):
-        #do as much checking as possible to make sure the socket is clean and ready to
-        #go back into the pool
-        if exc_type: #if any exception was raised, don't trust socket
-            killsock(self.sock) #(who knows what state the protocol is in)
-            return
-        if gevent.select.select([self.sock], [], [], 0)[0]:
-            killsock(self.sock)
-            return
-            #socket is readable means one of two things:
-            #1- left in a bad state (e.g. more data waiting)
-            #2- socket closed by remote (in which case read will return empty string)
-
-        self.pool.release(self.sock)
-
-
-
 
