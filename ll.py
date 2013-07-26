dccbd954cd380117af65d4bbf7a5410fb2bb7028
Chris Austin-Lane <caustinlane@corp.ebay.com>
2013-07-26 16:32:13 -0700

    more doc on lll

--- ll.py
+++ ll.py
@@ -4,6 +4,24 @@ Low-Level Logging
 A module to allow a ton of data (e.g. all SSL unencrypted and encrypted IO) to
 be logged but not actually slow the server down unless the thing is being traced
 or the whole server is logging super verbose.
+
+Use like:
+
+import ll
+
+ml = ll.LLogger()
+
+....
+
+ml.la("format string {0} {1}", var0, var1)  # always log
+
+ml.ld("format string 2 {0}", var0)  # log most often
+ml.ld("format string 3 {0}", var0)  # log most often
+ml.ld2("format string 4 {0}", var0)  # log less often
+ml.ld3("format string 5 {0}", var0)  # log only at highest verbosity
+
+caustinlane@paypal.com for details.
+
 """
 
 import inspect
@@ -25,8 +43,8 @@ _log_level = LOG_LEVELS['NONE']
 
 
 class LLogger(object):
-    """Instantiate this to get the module data about the caller"""
-    
+    """Instantiate this to get the logger object; it grabs module data"""
+
     def __init__(self):
         self.caller_mod = inspect.getmodule(inspect.stack()[1][0]).__file__.split(".")[-2].upper()
         self.la = self.log_always
@@ -35,12 +53,14 @@ class LLogger(object):
         self.ld3 = self.log_debug3
 
     def log_always(self, *args, **kw):
+        """Unconditionally log"""
         log_msgs[args[0]] += 1
         msg = apply(args[0].format, tuple(args[1:]))
         print "%s %s A:" % (datetime.now().strftime("%d/%H:%M:%S.%f"),
                             self.caller_mod), msg
 
     def log_debug(self, *args, **kw):
+        """Log only with -v"""
         log_msgs[args[0]] += 1
         if _log_level >= 1:
             msg = apply(args[0].format, tuple(args[1:]))
@@ -48,6 +68,7 @@ class LLogger(object):
                                 self.caller_mod), msg
                               
     def log_debug2(self, *args, **kw):
+        """Log only with -vv"""
         log_msgs[args[0]] += 1
         if _log_level >= 2:
             msg = apply(args[0].format, tuple(args[1:]))
@@ -55,6 +76,7 @@ class LLogger(object):
                                  self.caller_mod), msg
 
     def log_debug3(self, *args, **kw):
+        """Log only with -vvv"""
         log_msgs[args[0]] += 1
         if _log_level >= 3:
             msg = apply(args[0].format, tuple(args[1:]))
