a69758a06a70e05eab2f5bb3fe8af2b2c261e41d
Chris Austin-Lane <caustinlane@corp.ebay.com>
2013-09-27 09:42:50 -0700

    Count Q depth before apply, in case it runs fast.

--- async.py
+++ async.py
@@ -111,8 +111,8 @@ def cpu_bound(f):
                     def set_flag():
                         tlocals.in_cpu_thread = True
                     tlocals.cpu_thread.apply_e((Exception,), set_flag, (), {})
-                ret = tlocals.cpu_thread.apply_e((Exception,), in_thread, a, kw)
                 ctx.stats['cpu_bound.depth'].add(len(tlocals.cpu_thread))
+                ret = tlocals.cpu_thread.apply_e((Exception,), in_thread, a, kw)
                 ml.ld3("Enqueued to thread {0}/depth {1}", f.__name__, len(tlocals.cpu_thread))
         start = started[0]
         duration = curtime() - start
