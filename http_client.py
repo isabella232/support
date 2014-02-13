55fa75dc15144c2e0ad09bfb30e7bb7c9c7e7800
Mark Williams <mwilliams3@ebay.com>
2014-02-12 18:35:15 -0800

    urllib2 Request/addinfourl translation in infra.http_client

--- http_client.py
+++ http_client.py
@@ -8,12 +8,11 @@ http_client.request("get", "http://example.com/foo")
 import httplib
 from urlparse import urlparse, urlunparse
 import functools
+import urllib2
 
-import async
 import context
 
 from gevent import socket
-import OpenSSL.SSL
 
 
 # TODO: make and use a better HTTP library instead of wrapping httplib.
@@ -23,6 +22,7 @@ import OpenSSL.SSL
 
 
 class _GHTTPConnection(httplib.HTTPConnection):
+
     def __init__(self, host, port=None, protected=None, strict=None,
                  timeout=socket._GLOBAL_DEFAULT_TIMEOUT):
         httplib.HTTPConnection.__init__(self, host, port, strict, timeout)
@@ -30,21 +30,54 @@ class _GHTTPConnection(httplib.HTTPConnection):
 
     def connect(self):
         ctx = context.get_context()
-        self.sock = ctx.connection_mgr.get_connection((self.host, self.port), self.protected)
+        self.sock = ctx.connection_mgr.get_connection((self.host, self.port),
+                                                      self.protected)
         if self._tunnel_host:
             self._tunnel()
 
     def release_sock(self):
         if self._HTTPConnection__state == "Idle" and self.sock:
             context.get_context().connection_mgr.release_connection(self.sock)
-            self.sock = None    
+            self.sock = None
 
     def __del__(self):
         self.release_sock()
 
 
+def urllib2_request(u2req, timeout=None):
+    """\
+    Translate a urllib2.Request to something we can pass to our
+    request() function, and translate our Response to a
+    urllib2.addinfourl object
+    """
+    # TODO: proxy support?
+    method = u2req.get_method()
+    url = u2req._Request__original
+    body = u2req.get_data()
+    headers = dict(u2req.unredirected_hdrs)
+    headers.update(dict(k, v) for k, v in u2req.headers.items()
+                   if k not in headers)
+    try:
+        kwargs = {}
+        if timeout is not None:
+            kwargs['timeout'] = timeout
+        resp = request(method, url, body, headers, **kwargs)
+        hr = resp.http_response
+        hr.recv = hr.read
+        fp = socket._fileobject(hr, close=True)
+        aiu = urllib2.addinfourl(fp=fp,
+                                 headers=hr.msg,
+                                 url=resp.request.url)
+        aiu.code = hr.status
+        aiu.msg = hr.reason
+        return aiu
+    except ValueError as e:
+        raise urllib2.URLError(e.msg)
+
+
 def request(method, url, body=None, headers={},
-            literal=False, use_protected=False):
+            literal=False, use_protected=False,
+            timeout=socket._GLOBAL_DEFAULT_TIMEOUT):
     if method not in _HTTP_METHODS:
         raise ValueError("invalid http method {0}".format(method))
 
@@ -57,9 +90,10 @@ def request(method, url, body=None, headers={},
     except ValueError:
         port = 80 if parsed.scheme == 'http' else 443
 
-    protected = (parsed.scheme == 'https') and (True if use_protected else "PLAIN_SSL")
+    protected = (parsed.scheme == 'https') and (True if use_protected
+                                                else "PLAIN_SSL")
 
-    conn = _GHTTPConnection(domain, port, protected=protected)
+    conn = _GHTTPConnection(domain, port, protected=protected, timeout=timeout)
 
     selector = urlunparse(parsed._replace(scheme='', netloc=''))
 
@@ -85,10 +119,11 @@ def request(method, url, body=None, headers={},
 
     if body is not None:
         conn.send(body)
-    raw = conn.getresponse()
+    raw = conn.getresponse()    # does NOT hold a reference to the
+                                # HTTPConnection
     return Response(
         Request(method, url, headers, body),
-        raw.status, raw.getheaders(), raw.read())
+        raw.status, raw.getheaders(), raw)
 
 
 class Request(object):
@@ -103,11 +138,15 @@ class Request(object):
 
 
 class Response(object):
-    def __init__(self, request, status, headers, body):
+    def __init__(self, request, status, headers, http_response):
         self.request = request
         self.status = status
         self.headers = headers
-        self.body = body
+        self.http_response = http_response
+
+    @property
+    def body(self):
+        return self.http_response.read()
 
     def __repr__(self):
         return "<http_client.Response ({0}) {1} {2}>".format(
