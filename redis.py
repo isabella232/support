f9834023a46e7dc0433c919e52f420ca2cfda886
Kurt Rose <kurt@kurtrose.com>
2014-11-04 13:47:21 -0800

    migrated redis client to use buffered_socket.BufferedSocket instead of manual buffering

--- redis.py
+++ redis.py
@@ -2,7 +2,7 @@
 Quick and limited Redis client.
 '''
 import context
-import socket
+import buffered_socket
 
 
 class Client(object):
@@ -19,25 +19,23 @@ class Client(object):
         protocol used by Redis >= 1.2
         '''
         cm = context.get_context().connection_mgr
-        sock = cm.get_connection(self.address)
+        sock = buffered_socket.BufferedSocket(cm.get_connection(self.address))
         # ARRAY: first byte *, decimal length, \r\n, contents
         out = ['*' + str(len(commands))] + \
             ["${0}\r\n{1}".format(len(e), e) for e in commands]
         out = "\r\n".join(out) + "\r\n"
         sock.send(out)
-        resp = sock.recv(4096)
-        if resp.startswith("-"):  # error string
-            error, _ = _recv_until(sock, resp, '\r\n')
-            raise RedisError(error[1:])
-        elif resp.startswith('+'):  # simple string
-            resp, _ = _recv_until(sock, resp, '\r\n')
-            resp = resp[1:]
-        elif resp.startswith('$'):  # bulk string
-            length, rest = _recv_until(sock, resp, '\r\n')
-            length = int(length[1:])
+        fbyte = sock.peek(1)
+        if fbyte == "-":  # error string
+            raise RedisError(sock.recv_until('\r\n'))
+        elif fbyte == '+':  # simple string
+            resp = sock.recv_until('\r\n')[1:]
+        elif fbyte == '$':  # bulk string
+            length = int(sock.recv_until('\r\n')[1:])
             if length == -1:
-                return None
-            resp, _ = _recv_all(sock, rest, length)
+                resp = None
+            else:
+                resp = sock.recv_all(length)
         cm.release_connection(sock)
         return resp
 
@@ -50,62 +48,5 @@ class Client(object):
         return self.call("GET", key)
 
 
-# TODO: make this into a loop that consumes and parses data better
-def _parse(data):
-    if data[0] == '+':
-        assert data.endswith('\r\n')
-        return data[1:-2]
-    if data[0] == '$':
-        length, _, value = data[1:].partition('\r\n')
-        if length == "-1":
-            return None
-        return value[:int(length)]
-
-
 class RedisError(ValueError):
     pass
-
-
-def _recv_until(sock, sofar, marker):
-    'read off of socket and sofar until the marker is found'
-    chunks = []
-    nxt = sofar or sock.recv(32 * 1024)
-    if not sofar:
-        context.get_context().store_network_data(
-            (sock.getsockname(), sock.getpeername()), sock.fileno(), "IN", nxt)
-    while nxt and marker not in nxt:
-        chunks.append(nxt)
-        nxt = sock.recv(32 * 1024)
-        context.get_context().store_network_data(
-            (sock.getsockname(), sock.getpeername()), sock.fileno(), "IN", nxt)
-    if not nxt:
-        raise socket.error('connection was closed')
-    val, _, rest = nxt.partition(marker)
-    return ''.join(chunks) + val, rest
-
-
-def _recv_all(sock, sofar, size):
-    'read off of socket and sofar until size bytes have been read'
-    chunks = []
-    total_bytes = 0
-    nxt = sofar or sock.recv(32 * 1024)
-    if not sofar:
-        context.get_context().store_network_data(
-            (sock.getsockname(), sock.getpeername()), sock.fileno(), "IN", nxt)
-    while nxt:
-        total_bytes += len(nxt)
-        if total_bytes >= size:
-            break
-        chunks.append(nxt)
-        nxt = sock.recv(32 * 1024)
-        context.get_context().store_network_data(
-            (sock.getsockname(), sock.getpeername()), sock.fileno(), "IN", nxt)
-    else:
-        raise socket.error('connection was closed')
-    extra_bytes = total_bytes - size
-    if extra_bytes:
-        last, rest = nxt[:-extra_bytes], nxt[-extra_bytes:]
-    else:
-        last, rest = nxt, None
-    chunks.append(last)
-    return ''.join(chunks), rest
