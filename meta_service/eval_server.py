adb7e36df427be6dca7e7ef6d3d8b3f16d38fb7f
Kurt Rose <kurt@kurtrose.com>
2014-11-21 13:10:19 -0800

    fixing up eval_server

--- meta_service/eval_server.py
+++ meta_service/eval_server.py
@@ -2,6 +2,7 @@ import code
 import traceback
 import cgi
 import sys
+import json
 from StringIO import StringIO
 
 import clastic
@@ -39,12 +40,14 @@ def eval_command(request, eval_context, global_contexts):
                 ctx['sofar'] = []
                 buff = StringIO()
                 sys.stdout = buff
+                sys.stderr = buff
                 try:
                     exec cmd in ctx['locals']
+                    resp = buff.getvalue()
                 except Exception:
                     resp = traceback.format_exc()
                 sys.stdout = sys.__stdout__
-                resp = buff.getvalue()
+                sys.stderr = sys.__stderr__
             else:  # incomplete command
                 ctx['sofar'].append(line)
                 resp = "..."
@@ -54,10 +57,15 @@ def eval_command(request, eval_context, global_contexts):
         except (OverflowError, ValueError) as e:
             resp = repr(e)
             ctx['sofar'] = []
-        return clastic.Response(cgi.escape(resp))
+        resp = {
+            'complete': resp != '...',
+            'data': cgi.escape(resp) if resp != '...' else '',
+        }
+        return clastic.Response(json.dumps(resp), mimetype="application/json")
     finally:
         # try to really ensure stdout isn't left broken
         sys.stdout = sys.__stdout__
+        sys.stderr = sys.__stderr__
 
 
 # TODO: use a two column table for better cut + paste
@@ -74,17 +82,26 @@ CONSOLE_HTML = '''
         .cli_output {
             bottom: 0;
         }
-        #cli_input, #console {
+        #cli_input, #console, #prompt {
             font-size: 15px;
             font-family: "Lucida Console", Monaco, "Bitstream Vera Sans Mono", monospace;
+        }
+        .tr-even {
+            background: #FFFFFF;
+        }
+        .tr-odd {
+            background: #E0F8F7;
         }
     </style>
 </head>
 <body>
 
 <div style="position:absolute; bottom:0; width: 100%">
-<div id="console" style="overflow:scroll; height:400px; width: 100%"></div>
-&gt;&gt;&gt;<input type="text" id="cli_input" style="width: 50%"></input>
+<div id="console" style="overflow:scroll; height:400px; width: 100%">
+    <table id="console_out"></table>
+</div>
+<span id="prompt" style="width: 3em">&gt;&gt;&gt;</span>
+<input type="text" id="cli_input" style="width: 50%"></input>
 </div>
 
 <script>
@@ -94,21 +111,39 @@ $('#cli_input').keyup(function(event) {
     }
 });
 
-function console_append(val) {
-    $('#console').append('<div class="cli_output">' + val + '</div>');
+EVEN_ODD = true;
+
+function console_append(prompt, val) {
+    var rowclass = EVEN_ODD ? 'tr-even' : 'tr-odd';
+    $('#console').append(
+        '<tr class="' + rowclass + '"><td style="width: 3em">' + prompt +
+        '</td><td>' + val + '</td></tr>');
     $('#console').scrollTop($('#console')[0].scrollHeight);
+    EVEN_ODD = !EVEN_ODD;
 }
 
 function process_input() {
-    val = $('#cli_input').val();
-    console_append('&gt;&gt;&gt; ' + val);
+    var val = $('#cli_input').val();
+    console_append($("#prompt").text(), val.replace(/ /g, '&nbsp;'));
     $('#cli_input').val('');
     $.ajax({
             type: "POST",
             url: "CALLBACK_URL",
             data: {"command": val},
-            success: console_append
-        })
+            success: function(data) {
+                if(data.complete) {
+                    var prompt = ">>>";
+                } else {
+                    var prompt = "...";
+                }
+                $("#prompt").text(prompt);
+                if(data.data != '') {
+                    console_append(
+                        '',
+                        data.data.replace(/ /g, '&nbsp;').replace(/\\n/g, '<br/>'));
+                }
+            }
+        });
 }
 </script>
 </body>
