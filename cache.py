896464b1824f217559273019e315c1cc5a4b6fa5
Kurt Rose <kurt@kurtrose.com>
2014-12-03 17:32:17 -0800

    migrating cache.py to a stricter implementation

--- cache.py
+++ cache.py
@@ -1,50 +1,103 @@
-import itertools
 
 class Cache(object):
     'empty base class to enable isinstance(foo, cache.Cache)'
 
 
-class LRU(Cache):
+class LRUCache(Cache):
     '''
-    Extremely simple Leat Recently Used (LRU) eviction cache which ensures 
-    that the most recently accessed _size_ items are kept.
+    Implements an LRU cache based on a linked list.
+    Performance is about 1.1 microseconds per set/get on a core i7
     '''
-    def __init__(self, size):
-        self.size = size
-        self.data = {}
-        self.opnum = itertools.count(0)
-
-    def __setitem__(self, key, value):
-        self.data[key] = self.opnum.next(), value
-        if len(self.data) > 2 * self.size:
-            items = [(k, v) for k, v in self.data.items()]
-            items.sort(key=lambda e: e[1][0], reverse=True)
-            self.data = dict(items[:self.size])
+    def __init__(self, maxlen=10000):
+        self.map = {}
+        self.root = root = []
+        root[:] = [root, root]
+        self.maxlen = maxlen
 
     def __getitem__(self, key):
-        if key in self.data:
-            value = self.data[key][1]
-            self.data[key] = self.opnum.next(), value
-            return value
-        raise KeyError("key not found or expired")
+        val = self.map[key][3]
+        self[key] = val
+        return val
 
-    def __contains__(self, key): 
-        return key in self.data
+    def add(self, key, val):
+        # node[0] = prev; node[1] = next
+        root = self.root
+        if key in self.map:
+            # remove from map
+            link = self.map.pop(key)
+            # remove from list
+            link[0][1], link[1][0] = link[1], link[0]
+        else:
+            link = [None, None, key, val]
+        discard = None
+        if len(self.map) >= self.maxlen:
+            # pop and discard the oldest item
+            discard = root[0]
+            discard[0][1], root[0] = root, discard[0]
+            self.map.pop(discard[2])
+        # insert into map
+        self.map[key] = link
+        # insert into list
+        link[0], link[1] = root, root[1]
+        root[1][0] = link
+        root[1] = link
+        if root[0] is root:
+            root[0] = root[1]
+        if discard:
+            return discard[2], discard[3]
 
-    def __len__(self): 
-        return len(self.data)
+    __setitem__ = add
+
+    def pop(self, key):
+        # remove from map and list
+        link = self.map.pop(key)
+        link[0][1], link[1][0] = link[1], link[0]
+        return link[3]
+
+    def __contains__(self, key):
+        return key in self.map
+
+    def __len__(self):
+        return len(self.map)
 
     def items(self):
-        return [(k, self.data[k][1]) for k in self.data]
+        return [(k, self.map[k][3]) for k in self.map]
 
     def keys(self):
-        return self.data.keys()
+        return self.map.keys()
 
     def values(self):
-        return [e[1] for e in self.data.values()]
+        return [self.map[k][3] for k in self.map]
 
 
-class DefaultLRU(LRU):
+class SegmentedCache(Cache):
+    '''
+    Implements a Segmented LRU cache based on an LRU cache.
+    '''
+    def __init__(self, maxlen=10000):
+        self.probationary = LRUCache(maxlen / 2)
+        self.protected = LRUCache(maxlen / 2)
+
+    def __getitem__(self, key):
+        if key in self.protected.map:
+            # already protected, nothing to do
+            return self.protected[key]
+        if key in self.probationary.map:
+            # promote to protected
+            val = self.probationary.pop(key)
+            discard = self.protected.add(key, val)
+            if discard:
+                self.probationary.add(discard[0], discard[1])
+            return val
+        raise KeyError(key)
+
+    def add(self, key, val):
+        return self.probationary.add(key, val)
+
+    __setitem__ = add
+
+
+class DefaultLRU(LRUCache):
     '''
     An LRU which behaves like a collections.defaultdict on missing keys.
     '''
@@ -97,7 +150,7 @@ class DefaultEmptyCache(EmptyCache):
 
 
 if __name__ == "__main__":
-    c = LRU(3)
+    c = LRUCache(3)
     for i in range(10):
         c[i] = i
         c[0]  # keep 0 in top 3 most recent
