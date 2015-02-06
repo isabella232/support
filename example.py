11f88d970da46d4affb5d964789906dedfc06366
Mahmoud Hashemi <mahmoud@paypal.com>
2015-02-05 18:13:46 -0800

    example's working like a champ

--- example.py
+++ example.py
@@ -2,13 +2,15 @@
 import support
 from support import Group
 from clastic import Application
+from clastic import render_basic
 
+from support.server_group import SimpleSupportApplication
 
 def home_handler():
     return 'Hello!'
 
 
-app = Application([('/', home_handler)])
+app = Application([('/', home_handler, render_basic)])
 
 support.init()
 group = Group(wsgi_apps=[(app, ('0.0.0.0', 8888), False)])
