55c9ace297cb57a85ac3b499227b58fea01cb1e7
Kurt Rose <kurt@kurtrose.com>
2015-02-10 18:16:35 -0800

    added line cache mechanism to exceptions.py as first step towards generating stack traces from greenlet spawn chains

--- support/exceptions.py
+++ support/exceptions.py
@@ -5,6 +5,43 @@ that can be subclassed for user code or used in try/except blocks
 import sys
 import traceback
 
+import gevent
+
+
+class GLineCache(object):
+    'same idea as linecache.py module, but uses gevent primitives and thread/greenlet safe'
+    def __init__(self):
+        self.cache = {}
+
+    def getline(self, filename, lineno):
+        if filename not in self.cache:
+            self.update(filename)
+        lines = self.cache.get(filename, [])
+        try:
+            return lines[lineno]
+        except IndexError:
+            return ''
+
+    def update(self, filename):
+        def trypath(path):
+            try:
+                with open(path, 'Ur') as f:
+                    return f.readlines()
+            except IOError:
+                return None
+
+        lines = trypath(filename)
+        if lines is None:
+            # TODO: is loader.getsource() important here?
+            # ... in practice are there a significant number of libs
+            # distributed in zip or other non-source-file form?
+            for directory in sys.path:
+                lines = trypath(os.path.join(directory, filename))
+                if lines is not None:
+                    break
+        return lines
+
+
 class ASFError(Exception):
     """
     This exception is kind of nice because it can wrap an existing
